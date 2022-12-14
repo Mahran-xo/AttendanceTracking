e3c859436cc4a1b806301bf5644e4952407466ab
Mahran-xo <96589883+Mahran-xo@users.noreply.github.com>
2022-12-15 00:55:51 +0200

    yarab ne5las

--- services/AbsenceFormManagement.js
+++ services/AbsenceFormManagement.js
@@ -1,4 +1,3 @@
-const AttendanceModel = require('../models/Attendance');
 const AbsenceModel = require('../models/AbsenceForm');
 const ModuleModel = require('../models/Module');
 const StudentModel = require('../models/Student');
@@ -9,28 +8,27 @@ module.exports.SubmitAbscenceForm = async (AbsenceForm) => {
 
     const ABS = new AbsenceModel({
         studentId: AbsenceForm.id,
-        module:AbsenceForm.module,
-        reason:AbsenceForm.reason
-        
+        module: AbsenceForm.module,
+        reason: AbsenceForm.reason
     });
 
     try {
 
         const ABSsaved = await ABS.save();
-        const resp = await StudentModel.findOne({_id:ABS.studentId})
-        const modspec = await ModuleModel.findOne({moduleName:ABS.module})
+        const resp = await StudentModel.findById({ _id: ABS.studentId })
+        const modspec = await ModuleModel.findOne({ moduleName: ABS.module })
 
-        jsonData={
-            studentName:resp.name,
-            email:resp.email,
-            moduleCode:modspec.moduleCode,
-            assignedProfessor:modspec.assignedProfessor
+        jsonData = {
+            studentName: resp.name,
+            email: resp.email,
+            moduleCode: modspec.moduleCode,
+            assignedProfessor: modspec.assignedProfessor
         }
         await axios.post(process.env.ABSENCE, jsonData);
-        console.log({ABSsaved});
+        console.log({ ABSsaved });
         return ABSsaved;
 
-        
+
 
     }
 
@@ -43,16 +41,16 @@ module.exports.SubmitAbscenceForm = async (AbsenceForm) => {
 
 };
 
-module.exports.RetrieveAbsenceForms=async()=>{
+module.exports.RetrieveAbsenceForms = async () => {
 
-    try{
+    try {
         const ABS = await AbsenceModel.find().populate('studentId');
         return ABS
 
-        }
-        
-    
-    catch(error){
+    }
+
+
+    catch (error) {
         console.log(error);
         throw new Error('Could not Absence forms');
     }
