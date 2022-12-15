f7984bae6d09ec3cfe63c0a6bda9539d0cae9e5a
Mahran-xo <96589883+Mahran-xo@users.noreply.github.com>
2022-12-15 02:11:59 +0200

    Update Students.js

--- routes/Students.js
+++ routes/Students.js
@@ -6,7 +6,7 @@ const StudentValidator = require('../validators/Students');
 const StudentsRouter=Router();
 
 StudentsRouter.post('/',StudentValidator.validateStudents(),StudentsController.postStudent);
-StudentsRouter.post('/AssignToModule',StudentsController.AssignToModule);
+StudentsRouter.post('/AssignToModule/:id',StudentsController.AssignToModule);
 StudentsRouter.post('/RemoveStudentFromModule',StudentsController.RemoveStudentFromModule);
 StudentsRouter.get('/RetrieveAllStudents',StudentsController.RetrieveAllStudents);
 StudentsRouter.get('/:studentId',StudentsController.getStudent);
